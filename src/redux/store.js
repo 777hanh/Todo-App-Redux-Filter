bd46daa6d04793205c8f635c7e51afbdd36bd98b
nongdanxomdua <thanhthientran1511@gmail.com>
2022-05-11 17:06:58 +0700

    save state to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,31 @@
 import { createStore } from "redux";
 import rootReducer from './reducer';
+
+const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem('Todo-APP-State-Redux');
+        if (serializedState === null)
+            return undefined;
+        return JSON.parse(serializedState);
+    } catch (error) {
+        return undefined;
+    }
+}
+const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('Todo-APP-State-Redux', serializedState);
+    } catch (error) {
+        return undefined;
+    }
+}
+const peristedState = loadState();
 //store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, peristedState);
+
+store.subscribe(() => {
+    const state = store.getState();
+    saveState(state);
+})
 
 export default store
